e21dbeddcde015b208143085d4005e9721857a70
HendrikVE <hendrik1995@msn.com>
2017-06-30 15:00:26 +0200

    removed rest of code from spaceShip and defined tiles

--- Punktefresser/Texture.cpp
+++ Punktefresser/Texture.cpp
@@ -7,6 +7,10 @@ Texture::Texture(const char *source) {
     INFO(ss)
 
     this->textureSurface = IMG_Load(source);
+
+    std::stringstream ss2;
+    ss2 << "null: \"" << (this->textureSurface == NULL) << "\"";
+    INFO(ss2);
 }
 
 SDL_Surface *Texture::getSurface() {
--- Punktefresser/main.cpp
+++ Punktefresser/main.cpp
@@ -1,112 +1,9 @@
-#include <vector>
-#include <algorithm>
 #include "stdafx.h"
 
-const int X_SPEED = 15;
-const int Y_SPEED = 10;
+class TestEntity : public Entity {
 
-class Background : public Entity {
 public:
-    Background(float x, float y, Tile* tile) : Entity(x, y) {
-        this->tile = tile;
-    }
-    Tile* getTile() {
-        return tile;
-    }
-private:
-    Tile* tile;
-};
-
-class BackgroundScroller {
-public:
-    BackgroundScroller(Tile* entity, int vx) {
-        background1 = new Background(0, 0, entity);
-        background2 = new Background(background1->getWidth(), 0, entity);
-        this->vx = vx;
-    }
-
-    ~BackgroundScroller() {
-        delete background1;
-        delete background2;
-    }
-
-    void update() {
-        background1->setX(background1->getX() - vx);
-        background2->setX(background2->getX() - vx);
-        resetIfOutOfScreen(background1);
-        resetIfOutOfScreen(background2);
-    }
-
-    void render(Window* window) {
-        window->renderEntity(background1);
-        window->renderEntity(background2);
-    }
-
-private:
-    Background* background1;
-    Background* background2;
-    int vx = 1;
-
-    void resetIfOutOfScreen(Background* background) {
-        if (background->getX() <= -background->getWidth()) {
-            background->setX(background->getWidth());
-        }
-    }
-};
-
-class Shot : public Entity {
-public:
-    Shot(float x, float y, float movementX, float movementY) : Entity(x, y) {
-        Texture* texture_spaceships = new Texture ("assets/spaceships.png");
-        this->tile = new Tile(texture_spaceships, 32, 0, 32, 32);
-        this->movementX = movementX;
-        this->movementY = movementY;
-    }
-
-    Tile* getTile() {
-        return tile;
-    }
-
-    void update(std::vector<Entity*> gameObjects, Window window) {
-        setX(getX() + movementX);
-        setY(getY() + movementY);
-
-        //check bounds
-        if(isOutOfWindow(window)) {
-            INFO("out of window")
-
-            //TODO: remove this shot from gameObjects
-        }
-    }
-
-    /*bool checkCollision(std::vector<EnemySpaceShip*> spaceShips) {
-
-        for(EnemySpaceShip* spaceShip : spaceShips) {
-            if(SDL_HasIntersection(getRekt(), spaceShip->getRekt())) {
-                INFO("EXPLOSIOOOOOONNN")
-            }
-        }
-
-    }*/
-
-private:
-    Tile* tile;
-    float movementX;
-    float movementY;
-
-    bool isOutOfWindow(Window window) {
-
-        return
-                getX() > window.getWidth()
-                || getX() + getWidth() < 0
-                || getY() > window.getHeight()
-                || getY() + getHeight() < 0;
-    }
-};
-
-class SpaceShip : public Entity {
-public:
-    SpaceShip(float x, float y, Tile* tile) : Entity(x, y) {
+    TestEntity(float x, float y, Tile* tile) : Entity(x, y) {
         this->tile = tile;
     }
 
@@ -114,178 +11,68 @@ public:
         return tile;
     }
 
-    void move(int moveX, int moveY, Window window) {
-
-        //check bounds
-        float newPositionX = getX() + moveX;
-        float newPositionY = getY() + moveY;
-
-        if(newPositionX + getWidth() <= window.getWidth() && newPositionX >= 0) {
-            setX(newPositionX);
-        }
-
-        if(newPositionY + getHeight() <= window.getHeight() && newPositionY >= 0) {
-            setY(newPositionY);
-        }
-    }
-
-    virtual Shot* shoot() {
-        Shot* shot = new Shot(getX(), getY(), X_SPEED, 0);
-        return shot;
-    }
-
 private:
     Tile* tile;
-};
-
-class EnemySpaceShip : public SpaceShip {
-public:
-    EnemySpaceShip(float x, float y, Tile* tile) : SpaceShip(x, y, tile) {
-
-    }
-
-    void moveRandom(Window window) {
-
-        bool moveX = (bool) (rand() % 2);
-        bool moveY = (bool) (rand() % 2);
-
-        if(moveX) {
-            bool moveLeft = (bool) (rand() % 2);
-
-            if(moveLeft) {
-                move(-X_SPEED, 0, window);
-            }
-            else {
-                move(X_SPEED, 0, window);
-            }
-        }
-
-        if(moveY) {
-            bool moveTop = (bool) (rand() % 2);
-
-            if(moveTop) {
-                move(0, -Y_SPEED, window);
-            }
-            else {
-                move(0, Y_SPEED, window);
-            }
-        }
-    }
-
-    Shot* shoot() {
-        Shot* shot = new Shot(getX(), getY(), X_SPEED, 0);
-        return shot;
-    }
-
-private:
-
 };
 
 int main(int argc, char** argv) {
 
     srand (time(NULL));
 
-    Window window("SpaceShipRace", 800, 600);
+    int levelWidth = 800;
+    int levelHeigth = 600;
 
-    Texture texture_background("assets/background.png");
-    Texture texture_spaceships("assets/spaceships.png");
-    Texture texture_stars1("assets/stars1.png");
-    Texture texture_stars2("assets/stars2.png");
-    Texture texture_stars3("assets/stars3.png");
+    Window window("Punktefresser", levelWidth, levelHeigth);
 
-    Tile tile_background(&texture_background, 0, 0, 1024, 800);
-    Tile tile_spaceship(&texture_spaceships, 0, 32, 32, 32);
-    Tile tile_enemy_spaceship(&texture_spaceships, 0, 224, 32, 32);
-    Tile tile_stars1(&texture_stars1, 0, 0, 800, 600);
-    Tile tile_stars2(&texture_stars2, 0, 0, 800, 600);
-    Tile tile_stars3(&texture_stars3, 0, 0, 800, 600);
+    Texture mainTexture("assets/tiles.png");
 
-    Background background(0, 0, &tile_background);
+    Tile tilePlayerLookingTop(&mainTexture, 128, 32, 32, 32);
+    Tile tilePlayerLookingBot(&mainTexture, 96, 32, 32, 32);
+    Tile tilePlayerLookingLeft(&mainTexture, 128, 0, 32, 32);
+    Tile tilePlayerLookingRight(&mainTexture, 96, 0, 32, 32);
 
-    BackgroundScroller scroller1(&tile_stars1, 5);
-    BackgroundScroller scroller2(&tile_stars2, 4);
-    BackgroundScroller scroller3(&tile_stars3, 2);
+    Tile tileEnemy(&mainTexture, 160, 0, 32, 32);
 
-    std::vector<BackgroundScroller*> backgroundScroller;
-    backgroundScroller.push_back(&scroller1);
-    backgroundScroller.push_back(&scroller2);
-    backgroundScroller.push_back(&scroller3);
+    Tile tileFruitBanana(&mainTexture, 0, 64, 32, 32);
+    Tile tileFruitPeach(&mainTexture, 32, 64, 32, 32);
+    Tile tileFruitGrapes(&mainTexture, 64, 64, 32, 32);
+    Tile tileFruitApple(&mainTexture, 96, 64, 32, 32);
+    Tile tileFruitMelon(&mainTexture, 128, 64, 32, 32);
+    Tile tileFruitPineapple(&mainTexture, 160, 64, 32, 32);
+    Tile fruitTiles[] = {tileFruitBanana, tileFruitPeach, tileFruitGrapes, tileFruitApple, tileFruitMelon, tileFruitPineapple};
 
-    SpaceShip ship(32, 300, &tile_spaceship);
+    Tile tileWall(&mainTexture, 0, 0, 32, 32);
 
-    EnemySpaceShip enemyShip1(650, 100, &tile_enemy_spaceship);
-    EnemySpaceShip enemyShip2(550, 200, &tile_enemy_spaceship);
-    EnemySpaceShip enemyShip3(450, 300, &tile_enemy_spaceship);
-    EnemySpaceShip enemyShip4(550, 400, &tile_enemy_spaceship);
-    EnemySpaceShip enemyShip5(650, 500, &tile_enemy_spaceship);
+    Tile tilePoint(&mainTexture, 64, 0, 32, 32);
 
-    std::vector<EnemySpaceShip*> enemies;
-    enemies.push_back(&enemyShip1);
-    enemies.push_back(&enemyShip2);
-    enemies.push_back(&enemyShip3);
-    enemies.push_back(&enemyShip4);
-    enemies.push_back(&enemyShip5);
-
-    std::vector<Entity*> gameObjects;
+    TestEntity testEntity(64, 64, &tilePoint);
 
     InputHandler inputHandler;
     bool running = true;
     Timer timer;
 
-    const int MAX_CYCLES = 7;
-    int cyclesToWaitBeforeUpdateMap = MAX_CYCLES;
-
     while (running) {
 
-        if(--cyclesToWaitBeforeUpdateMap == -1) {
-            cyclesToWaitBeforeUpdateMap = MAX_CYCLES;
-        }
-
         // render entities
-        window.renderEntity(&background);
-
-        window.renderEntity(&ship);
-
-        for(BackgroundScroller* scroller : backgroundScroller) {
-            scroller->render(&window);
-            scroller->update();
-        }
-
-        for(EnemySpaceShip* enemy : enemies) {
-            window.renderEntity(enemy);
-            if(cyclesToWaitBeforeUpdateMap == 0) {
-                enemy->moveRandom(window);
-            }
-        }
-
-        for(Entity* entity : gameObjects) {
-            window.renderEntity(entity);
-
-            //trying to cast to shot
-            if(Shot* shot = dynamic_cast<Shot*>(entity)) {
-                shot->update(gameObjects, window);
-                //shot->checkCollision((std::vector<EnemySpaceShip*>)enemies);
-            }
-        }
+        window.renderEntity(&testEntity);
 
         // input
         while (inputHandler.pollEvent()) {
 
             if (inputHandler.isKeyPressed(SDLK_w)) {
-                ship.move(0, -Y_SPEED, window);
+
             }
             if (inputHandler.isKeyPressed(SDLK_s)) {
-                ship.move(0, Y_SPEED, window);
+
             }
             if (inputHandler.isKeyPressed(SDLK_a)) {
-                ship.move(-X_SPEED, 0, window);
+
             }
             if (inputHandler.isKeyPressed(SDLK_d)) {
-                ship.move(X_SPEED, 0, window);
+
             }
             if (inputHandler.isKeyPressed(SDLK_SPACE)) {
-                Shot* shot = ship.shoot();
-                gameObjects.push_back((Entity*)shot);
+
             }
 
             if (inputHandler.isQuitEvent()) {
