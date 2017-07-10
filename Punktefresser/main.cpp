db70dc2acecad7af71aae6154b9148ba23197a46
Lausi95 <tomlausmann@gmail.com>
2017-07-10 18:33:53 +0200

    entities created ONCES and fruit tiles are random

--- Punktefresser/main.cpp
+++ Punktefresser/main.cpp
@@ -42,83 +42,72 @@ PacmanTiles pacmanTiles = {
 
 Pacman pacman(0, 0, &pacmanTiles);
 
-std::vector <Enemy*> enemies;
+std::vector<Enemy> enemies;
+std::vector<StaticEntity> fruits;
+std::vector<StaticEntity> environment;
 
-void renderMap(Window window, LevelMap levelMap) {
-
+void initializeEntities(LevelMap levelMap) {
     for(int row = 0; row < levelMap.getRowCount(); row++) {
         for(int column = 0; column < levelMap.getColumnCount(); column++) {
-
-            Entity* entity;
-
-            //drawing background in every case (transperant fruits etc...)
-            entity = new StaticEntity(column * 32, row *32, &tileNormalBackground);
-            window.renderEntity(entity);
-
-            Field field = levelMap.getFieldAt(column, row);
-            switch(field) {
-
+            switch(levelMap(column, row)) {
                 case Field::FloorWithPoint:
-                    entity = new StaticEntity(column * 32, row *32, &tilePoint);
+                    environment.push_back(StaticEntity(column * 32, row *32, &tilePoint));
                     break;
-
                 case Field::Floor:
-                    entity = new StaticEntity(column * 32, row * 32, &tileNormalBackground);
+                    environment.push_back(StaticEntity(column * 32, row * 32, &tileNormalBackground));
                     break;
-
                 case Field::Wall:
-                    entity = new StaticEntity(column * 32, row *32, &tileWall);
+                    environment.push_back(StaticEntity(column * 32, row *32, &tileWall));
                     break;
-
                 case Field::Fruit:
-                    // entity = new Fruit(column * 32, row *32, &fruitTiles[rand() & 6]);
-                    entity = new StaticEntity(column * 32, row *32, &tileFruitApple);
+                    fruits.push_back(StaticEntity(column * 32, row *32, &fruitTiles[rand() & 6]));
+                    environment.push_back(StaticEntity(column * 32, row * 32, &tileNormalBackground));
                     break;
-
                 case Field::Player:
                     pacman.setX(column * 32);
                     pacman.setY(row * 32);
+                    environment.push_back(StaticEntity(column * 32, row * 32, &tileNormalBackground));
                     levelMap.setField(column, row, Field::FloorWithPoint);
                     break;
-
                 case Field::Enemy:
-                    enemies.push_back(new Enemy(column * 32, row *32, &tileEnemy));
+                    environment.push_back(StaticEntity(column * 32, row * 32, &tileNormalBackground));
+                    enemies.push_back(Enemy(column * 32, row *32, &tileEnemy));
                     levelMap.setField(column, row, Field::FloorWithPoint);
                     break;
             }
-
-            window.renderEntity(entity);
         }
     }
 }
 
 int main(int argc, char** argv) {
-
-    LevelMap levelMap = LevelMap::load("levels/default.lvl");
-
     srand (time(NULL));
 
+    // load level
+    LevelMap levelMap = LevelMap::load("levels/default.lvl");
+
+    // initialize entities
+    initializeEntities(levelMap);
+
+    // initialize window
     int levelWidth = levelMap.getColumnCount() * 32;
     int levelHeigth = levelMap.getRowCount() * 32;
-
     Window window("Punktefresser", levelWidth, levelHeigth);
 
+    // initialize input handler
     InputHandler inputHandler;
     bool running = true;
     Timer timer;
 
+
     while (running) {
-
-        // render entities
-        //window.renderEntity(pacman);
-
-        renderMap(window, levelMap);
-
+        // render
+        for (StaticEntity entity : environment)
+            window.renderEntity(&entity);
+        for (StaticEntity fruit : fruits)
+            window.renderEntity(&fruit);
+        for (Enemy entity : enemies)
+            window.renderEntity(&entity);
         window.renderEntity(&pacman);
-
-        for(Enemy* enemy : enemies) {
-            window.renderEntity(enemy);
-        }
 
         // input
         while (inputHandler.pollEvent()) {
@@ -141,8 +130,8 @@ int main(int argc, char** argv) {
                 running = false;
             }
         }
-        // update
 
+        // update
         pacman.tryApplyDirection(levelMap);
         pacman.move(levelMap);
 
