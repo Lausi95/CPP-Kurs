6d661c7baf0a9220a9eda5f392c61443d1ac70c5
Lausi95 <tomlausmann@gmail.com>
2017-07-16 23:25:03 +0200

    added simpler game engine

---
+++ BurningKiddies/include/BurnEngine.h
@@ -0,0 +1,76 @@
+#ifndef BURNINGKIDDIES_BURNENGINE_H
+#define BURNINGKIDDIES_BURNENGINE_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include <memory>
+
+namespace burnengine {
+
+    void initialize();
+
+
+    struct Texture {
+        SDL_Surface* surface;
+        int cellWidth;
+        int cellHeight;
+
+        Texture(const char* file, int cellWidth, int cellHeight);
+        ~Texture();
+    };
+
+
+    class Tile {
+    private:
+        Texture* texture;
+        std::unique_ptr<SDL_Rect> rect;
+
+    public:
+        Tile(Texture& texture, int cell_x, int cell_y, int cell_w = 1, int cell_h = 1);
+
+        inline Texture* getTexture() { return texture; }
+        inline SDL_Rect* getRect() { return rect.get(); }
+    };
+
+
+    class Entity {
+    private:
+        std::unique_ptr<SDL_Rect> rect;
+
+    public:
+        Entity(int x, int y);
+
+        virtual Tile* getTile() = 0;
+        inline SDL_Rect* getRect() { return rect.get(); }
+    };
+
+
+    class Game {
+    private:
+        SDL_Window* wnd;
+        SDL_Surface* wnd_surface;
+        SDL_Event event;
+        bool running = true;
+        unsigned long long ticks = 0;
+
+        bool keys[1024];
+
+    public:
+        Game(const char* title, int width, int height);
+        ~Game();
+
+        void update();
+        inline bool isRunning() const { return running; }
+        inline const unsigned long long getTicks() const { return ticks; }
+
+        void render(Entity& entity);
+        void resetTicks();
+
+        bool isKeyDown(const SDL_Keycode& keycode) const;
+    };
+
+}
+
+#endif
--- BurningKiddies/src/main.cpp
+++ BurningKiddies/src/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <BurnEngine.h>
+
+#include <SDL2/SDL.h>
+
+class SimpleEntity : public burnengine::Entity {
+    burnengine::Tile* tile;
+
+public:
+    SimpleEntity(burnengine::Tile& tile, int x, int y) : Entity(x, y) {
+        this->tile = &tile;
+    }
+
+    burnengine::Tile* getTile() {
+        return tile;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    using namespace burnengine;
+
+    initialize();
+    Game game("Burning Kiddies", 400, 300);
+
+    Texture dirtTexture("assets/images/tiles/floor_dirt.jpg", 32, 32);
+    Tile dirtTile(dirtTexture, 0, 0);
+    SimpleEntity entity(dirtTile, 0, 0);
+
+    while (game.isRunning()) {
+        game.update();
+
+        if (game.getTicks() % 1000 == 0) {
+            if (game.isKeyDown(SDLK_w)) {
+                entity.getRect()->y--;
+            }
+
+            if (game.isKeyDown(SDLK_a)) {
+                entity.getRect()->x--;
+            }
+
+            if (game.isKeyDown(SDLK_s)) {
+                entity.getRect()->y++;
+            }
+
+            if (game.isKeyDown(SDLK_d)) {
+                entity.getRect()->x++;
+            }
+        }
+
+        game.render(entity);
+    }
+
     return 0;
 }
